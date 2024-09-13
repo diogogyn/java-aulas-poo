37404ebcf9ce4ebfbf1662ed783764ef6d96b2f4
Diogo O Santos <diogogyn100@gmail.com>
2024-09-13 11:30:40 -0300

    Associando valores a objetos

--- src/main/java/br/sesi/poo/cabeca/Cabeca.java
+++ src/main/java/br/sesi/poo/cabeca/Cabeca.java
@@ -1,13 +1,15 @@
 package br.sesi.poo.cabeca;
 
 public class Cabeca {
-    public Olho olho;
+    public Olho olhoEsquerdo;
+    public Olho olhoDireito;
     public Orelha orelhao;
     public Nariz nariz;
     public Boca boca;
 
     public Cabeca(){
-        this.olho = new Olho();
+        this.olhoEsquerdo = new Olho();
+        this.olhoDireito = new Olho();
         this.orelhao = new Orelha();
         this.nariz = new Nariz();
         this.boca = new Boca();
